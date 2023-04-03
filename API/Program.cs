10fb00e42fc4c47fc7b158026b3004f4afefee23
Osman Tarık CAMCI <429883@egm.gov.tr>
2023-04-03 17:59:46 +0300

    cors added

--- API/Program.cs
+++ API/Program.cs
@@ -2,9 +2,8 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddCors();
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 // app.UseHttpsRedirection(); //our app always run https dediği için bu çıkardı.
 
 // app.UseAuthorization();
