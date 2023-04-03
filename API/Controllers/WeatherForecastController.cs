365d8274adb52937b0b7a533d87fe91eaf24271b
Osman Tarık CAMCI <429883@egm.gov.tr>
2023-04-03 10:48:22 +0300

    trash commit

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -21,12 +21,14 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        // {
+        //     Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //     TemperatureC = Random.Shared.Next(-20, 55),
+        //     Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        // })
+        // .ToArray();
+
+        return new List<WeatherForecast>() { new WeatherForecast { Date = DateOnly.FromDateTime(DateTime.Now), Summary = "new summary", TemperatureC = 70 } };
     }
 }
--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,9 +1,20 @@
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext:DbContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DataContext() => this.ChangeTracker.LazyLoadingEnabled = false;
+
+        public virtual DbSet<AppUser> AppUsers { get; set; }
+
         
+
     }
 }
