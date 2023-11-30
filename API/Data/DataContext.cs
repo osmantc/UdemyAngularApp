10e779984e4b4319e8246b49d6c3b5dd51418d83
osmantc <blackcalvus06@hotmail.com>
2023-11-30 23:34:06 +0300

    angular 17 geçiş öncesi save

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -12,9 +12,10 @@ namespace API.Data
 
         public DataContext() => this.ChangeTracker.LazyLoadingEnabled = false;
 
+
+
         public virtual DbSet<AppUser> AppUsers { get; set; }
-
-        
-
+        // public virtual DbSet<Photo> Photos { get; set; } //FIXME: tek başına bir photos nesnesi yoktur, mutlaka appuser ile alakalıdır.
+        //FIXME: o yüzden direkt db'ye eklenmeyeceğine göre (relationla appUser üzerinden eklenebilir sadece) Photos için dbset'e gerek yoktur.
     }
 }
